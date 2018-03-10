2b8db7c19d5b50d7e22bbe48367c127ac4d4c0ba
Georgiy Starkov <gstarkov658@gmail.com>
2018-03-11 02:42:30 +0300

    Fix title

--- src/Card/card.js
+++ src/Card/card.js
@@ -9,7 +9,7 @@ const Card = (props) => {
 		classOption = `card__option card__option_size_${props.size}`;
 	}
 	return(
-		<section
+		<article
 			className={`item item_size_${props.size} card card_size_${props.size}`}>
 			<h2 
 				style={{color: props.titleColor}}
@@ -44,7 +44,7 @@ const Card = (props) => {
 					<path className="heart" d="M8 3.2C7.4-0.3 3.2-0.8 1.4 1 -0.5 2.9-0.5 5.8 1.4 7.7 1.9 8.2 6.9 13 6.9 13 7.4 13.6 8.5 13.6 9 13L14.5 7.7C16.5 5.8 16.5 2.9 14.6 1 12.8-0.7 8.6-0.3 8 3.2Z"></path>
 				</svg>
 			</div>
-		</section>
+		</article>
 	)
 }
 
